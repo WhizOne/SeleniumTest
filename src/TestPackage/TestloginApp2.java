7034855453a89acdae7a0ee4eba35c2283d25f0f
gs35813 <gs35813@gmail.com>
2016-03-17 12:23:55 +0530

    Add the Project in the repo

---
+++ src/TestPackage/TestloginApp2.java
@@ -0,0 +1,43 @@
+package TestPackage;
+import java.io.IOException;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import TestUtilityPackage.CommonMethod;
+import TestUtilityPackage.LoginIntoApp;
+public class TestloginApp2 {
+	public WebDriver driver;
+	private String Pathoftestdata =CommonMethod.path;
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Roshan\\Desktop\\chromedriver.exe");
+		driver = new ChromeDriver();
+		//driver = new  FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+		}
+	@Test
+	public void LoginIntoApp2() throws InterruptedException, IOException{
+		
+			 String[][] getit = CommonMethod.GetValue(Pathoftestdata, "Login",1);
+			 String AppUrl = getit[0][0]; 
+			 String Username= getit[0][1];  
+			 String Password=getit[0][2];
+			 String ExpectedResult=getit[0][3]; 
+			 LoginIntoApp.LogIn(driver, AppUrl, Username, Password);
+		     String ActualResult =driver.findElement(By.xpath("html")).getText();	
+		     System.out.println("The Actual Result is = "+ActualResult);
+		     Assert.assertEquals(ActualResult,ExpectedResult);
+		}
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+		}
+	}
